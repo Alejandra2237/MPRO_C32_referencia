09b67186296b47a3f94620cf6e9f352e26fd9058
Alejandra2237 <84894155+Alejandra2237@users.noreply.github.com>
2022-12-07 13:54:08 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -67,9 +67,9 @@ function setup() {
   rightWalldown.shapeColor="#44C96C";
   
 // Paredes horizontales izquierdas
-  horizontalLeft1 = createSprite(75,160,150,50);
+  horizontalLeft1 = createSprite(75,157,150,45);
   horizontalLeft1.shapeColor="#CAFDFF";
-  horizontalLeft2 = createSprite(75,230,150,50);
+  horizontalLeft2 = createSprite(75,233,150,45);
   horizontalLeft2.shapeColor="#E963C2";
   horizontalLeft3 = createSprite(200,40,120,20);
   horizontalLeft3.shapeColor="#F13652";
@@ -87,9 +87,9 @@ function setup() {
   horizontalLeft9.shapeColor="#21C3D8";
 
 //Paredes horizontales Derechas
-  horizontalRight1 = createSprite(525,160,150,50);
+  horizontalRight1 = createSprite(525,157,150,45);
   horizontalRight1.shapeColor="#CAFDFF";
-  horizontalRight2 = createSprite(525,230,150,50);
+  horizontalRight2 = createSprite(525,233,150,45);
   horizontalRight2.shapeColor="#E963C2";
   horizontalRight3 = createSprite(400,40,120,20);
   horizontalRight3.shapeColor="#F13652";
